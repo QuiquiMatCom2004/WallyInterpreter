44047ada2cfafc10df1f571b4f7a6df6c1d637e4
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-05-17 13:03:13 -0400

    Abstraction of State

---
+++ Components/Interpreter/Automaton/IState.cs
@@ -0,0 +1,17 @@
+﻿namespace WallyInterpreter.Components.Interpreter.Automaton
+{
+    public interface IState<T>
+    {
+        string ID();
+        bool IsAccepting();
+        bool IsFault();
+        bool HasTransition(T symbol);
+        IState<T>[] Epsilons();
+        IState<T> Next(T symbol);
+        void AddTransition(T symbol,IState<T> state);
+
+        void AddEpsilon(IState<T> state);
+
+        IState<T>[] Clousure();
+    }
+}
