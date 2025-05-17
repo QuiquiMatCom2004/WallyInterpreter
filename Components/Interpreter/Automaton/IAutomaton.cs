f35e2bef413bd3ac5935f4755f0a5e8af68375f5
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-05-17 13:03:43 -0400

    Abstraction of Automaton

---
+++ Components/Interpreter/Automaton/IAutomaton.cs
@@ -0,0 +1,17 @@
+﻿namespace WallyInterpreter.Components.Interpreter.Automaton
+{
+    public interface IAutomaton<T>
+    {
+        IState<T> Start();
+        IState<T>[] Finals();
+        IState<T>[] States();
+        bool IsDeterministic();
+        IAutomaton<T> ToDeterministic();
+        void Walk(T symbol);
+        IState<T> CurrentState();
+        void AddState(IState<T> state);
+
+        T[] Alphabet();
+        void Restart();
+    }
+}
