dd0cbd56bb3bfdd433a38ac2546eef448ec0ba7c
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-05-17 20:39:34 -0400

    Add the Automaton Errors

---
+++ Components/Interpreter/Errors/Automaton Errors.cs
@@ -0,0 +1,28 @@
+﻿namespace WallyInterpreter.Components.Interpreter.Errors
+{
+    public class FaultNodeException : Exception
+    {
+        string mensaje = "This is a Fault Node";
+
+        public FaultNodeException()
+        {
+            Console.WriteLine(mensaje);
+        }
+
+        public FaultNodeException(string? message) : base(message)
+        {
+            if (message == null) message = mensaje;
+        }
+    }
+    public class NFA_AutomatonException:Exception
+    {
+        public NFA_AutomatonException()
+        {
+        }
+
+        public NFA_AutomatonException(string? message) : base(message)
+        {
+            if (message == null) message = "This Automaton is NonDeterministic, make it Deterministic First";
+        }
+    }
+}
