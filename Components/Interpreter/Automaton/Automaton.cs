33b44fe71d31b7b6eaac793fc1fa2411508bc907
QuiquiMatCom2004 <kikialeglez@gmail.com>
2025-05-17 20:37:32 -0400

    Documentando la clase state

---
+++ Components/Interpreter/Automaton/Automaton.cs
@@ -0,0 +1,98 @@
+﻿namespace WallyInterpreter.Components.Interpreter.Automaton
+{
+    public class Automaton<T> : IAutomaton<T>
+    {
+        private IState<T> _start;
+        private IState<T> _current;
+        private List<IState<T>> _states;
+        private T[] _alphabet;
+
+        public Automaton(IState<T> start, List<IState<T>> states,T[] alphabet) { 
+            _start = start;
+            _current = start;
+            _states = states;
+            _alphabet = alphabet;
+        }
+        public void AddState(IState<T> state)
+        {
+            _states.Add(state);
+        }
+
+        public T[] Alphabet()
+        {
+            return _alphabet;
+        }
+
+        public IState<T> CurrentState()
+        {
+            return _current;
+        }
+
+        public IState<T>[] Finals()
+        {
+            var finals = from state in _states where state.IsAccepting() select state;
+            return finals.ToArray();
+        }
+
+        public bool IsDeterministic()
+        {
+            var count = from state in _states where state.Epsilons().Length > 0 select state;
+            return count.Count() == 0;
+        }
+
+        public void Restart()
+        {
+            _current = _start;
+        }
+
+        public IState<T> Start()
+        {
+            return _start;
+        }
+
+        public IState<T>[] States()
+        {
+            return _states.ToArray();
+        }
+
+        public IAutomaton<T> ToDeterministic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Walk(T symbol)
+        {
+            if (_current.IsFault()) throw new Exception("This is a Fault Node");
+            if (!IsDeterministic())
+                throw new Exception("This Automaton is NonDeterministic, make it Deterministic First");
+            _current = _current.Next(symbol);
+        }
+
+        private struct AuxStructToDeterministic
+        {
+            public string id;
+            public IState<T>[][] states;
+            public Dictionary<string, Dictionary<T, string>> transition;
+        }
+        private Dictionary<string, IState<T>[]> GetClousures()
+        {
+            Dictionary<string, IState<T>[]> result = new Dictionary<string, IState<T>[]>();
+            foreach (var state in _states)
+            {
+                result[state.ID()] = state.Clousure();
+            }
+            return result;
+        }
+        private AuxStructToDeterministic AuxDeterministic()
+        {
+            var initial = _start.Clousure();
+            var clousures = GetClousures();
+            List<IState<T>[]> states = new List<IState<T>[]>();
+            states.Add(initial);
+            Dictionary<string, Dictionary<T, string>> transition = new Dictionary<string, Dictionary<T, string>>();
+            bool state_added = true;
+
+            throw new Exception();
+        }
+    }
+}
--- Components/Interpreter/Automaton/State.cs
+++ Components/Interpreter/Automaton/State.cs
@@ -18,17 +18,27 @@ namespace WallyInterpreter.Components.Interpreter.Automaton
             _accepting = accepting;
             _fault = fault;
         }
-
+        /// <summary>
+        /// Add a epsilon transition pointing the state getted
+        /// </summary>
+        /// <param name="state">the state to link</param>
         public void AddEpsilon(IState<T> state)
         {
             _epsilon.Add(state);
         }
-
+        /// <summary>
+        /// Add transition pointing a state when you read the symbol
+        /// </summary>
+        /// <param name="symbol">State to move</param>
+        /// <param name="state"> Symbol to read</param>
         public void AddTransition(T symbol, IState<T> state)
         {
             _transition[symbol] = state;
         }
-
+        /// <summary>
+        /// return all the states able to arrive with a epsilon transition more this state
+        /// </summary>
+        /// <returns>Array of states</returns>
         public IState<T>[] Clousure()
         {
             List<IState<T>> clousure = new List<IState<T>>() { this};
@@ -38,14 +48,21 @@ namespace WallyInterpreter.Components.Interpreter.Automaton
             }
             return clousure.ToArray();
         }
-
+        /// <summary>
+        /// return all the states able to arrive with a epsilon transition
+        /// </summary>
+        /// <returns></returns>
         public IState<T>[] Epsilons()
         {
             if (_fault)
                 return new IState<T>[] {};
             return _epsilon.ToArray();
         }
-
+        /// <summary>
+        /// Says if this state have a transition for the symbol
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
         public bool HasTransition(T symbol)
         {
             if(_fault)return false;
@@ -53,22 +70,35 @@ namespace WallyInterpreter.Components.Interpreter.Automaton
                 return true;
             return false;
         }
-
+        /// <summary>
+        /// return the id 
+        /// </summary>
+        /// <returns></returns>
         public string ID()
         {
             return _id;
         }
-
+        /// <summary>
+        /// return if is final state
+        /// </summary>
+        /// <returns>true is final</returns>
         public bool IsAccepting()
         {
             return _accepting;
         }
-
+        /// <summary>
+        /// return if is faul state
+        /// </summary>
+        /// <returns>true is fault</returns>
         public bool IsFault()
         {
             return _fault;
         }
-
+        /// <summary>
+        /// return the next state for the symbol
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns></returns>
         public IState<T> Next(T symbol)
         {
             if (_fault) return this;
